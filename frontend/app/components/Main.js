0d9ff6eb54c5120e080b43f36b3833108e57b0b6
hiogawa <hi.ogawa.zz@gmail.com>
2015-12-24 15:16:54 +0900

    rename jsx file from js file

--- frontend/app/components/Main.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export var Main = React.createClass({
-  render() {
-    return (
-      <div> Hello World </div>
-    );
-  }
-});
---
+++ frontend/app/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+require("!style!css!less!bootstrap/less/bootstrap.less");
+
+export var Main = React.createClass({
+  render() {
+    return (
+      <div className="text-center"> Hello World </div>
+    );
+  }
+});
